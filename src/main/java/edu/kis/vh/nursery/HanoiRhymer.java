4ef7c63d280ae87de3528a964534b6cf42987b8a
Konrad <nowakkonrad25@wp.pl>
2022-01-07 15:55:33 +0100

    1.3.2 Poprawa błędów zwiazanych z formatowaniem i dodanie komentarzy

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,24 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 }
+
+//Pytanie: Które wiersze w klasie HanoiRhymer były źle sformatowane?
+//
+//Wiersze o numerach 5, 12, 14, 15
+//
+//Zweryfikuj działanie kombinacji klawiszy alt + ← oraz alt + →
+//
+//Kombinacja ta pozwala na przełączanie się pomiędzy otwartymi plikami
