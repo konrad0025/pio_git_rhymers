8eabaa3e079a50910ff791b3b1e335f82b16185b
Adam Nieśmiałek <228811@edu.p.lodz.pl>
2022-01-10 11:12:05 +0100

    1.4 stale 1 - Dodanie stalych liczb

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,14 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int FULL_VALUE = 11;
+    public static final int TOTAL_VALUE = -1;
+    public static final int ERROR_RETURN = -1;
+    public static final int SIZE = 12;
 
-    public int total = -1;
+    private int[] numbers = new int[SIZE];
+
+    public int total = TOTAL_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +17,23 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+
+        return total == TOTAL_VALUE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL_VALUE;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERROR_RETURN;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERROR_RETURN;
         return numbers[total--];
     }
 
