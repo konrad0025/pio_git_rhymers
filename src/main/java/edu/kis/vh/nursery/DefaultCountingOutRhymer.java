57952a1b00fbfcdb16f05a8f39f3da824d4e8702
Konrad <nowakkonrad25@wp.pl>
2022-01-23 22:02:27 +0100

    1.4 accessory 2 utworzenie gettera dla pola total w klasie DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -11,6 +11,10 @@ public class DefaultCountingOutRhymer {
 
     private int total = TOTAL_VALUE;
 
+    public int getTotal(){
+        return total;
+    }
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
