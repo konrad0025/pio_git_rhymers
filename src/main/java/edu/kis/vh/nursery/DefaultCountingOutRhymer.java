d87480c6df6c1687ce6919f63b142158f998cc4e
Konrad <nowakkonrad25@wp.pl>
2022-01-23 22:00:28 +0100

    1.4 accessory 1 zmiana modyfikatorów public na private w polach klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,14 +2,14 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int FULL_VALUE = 11;
-    public static final int TOTAL_VALUE = -1;
-    public static final int ERROR_RETURN = -1;
-    public static final int SIZE = 12;
+    private static final int FULL_VALUE = 11;
+    private static final int TOTAL_VALUE = -1;
+    private static final int ERROR_RETURN = -1;
+    private static final int SIZE = 12;
 
     private final int[] numbers = new int[SIZE];
 
-    public int total = TOTAL_VALUE;
+    private int total = TOTAL_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
